9828668c24d25164bf8103fbde3a864921432a28
Luis Luvia <lluvia@marblelang.net>
2023-08-30 19:20:51 -0500

    Fix mobile formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,11 @@
   <title>Marble Language</title>
 </svelte:head>
 
-<div class="w-screen h-screen bg-jet">
-  <div class="flex flex-col items-center justify-center h-full space-y-8">
+<div class="w-screen h-screen bg-jet ">
+  <div class="flex flex-col items-center justify-center h-full space-y-8 px-8 sm:px-0">
     <img alt="Marble logo" src={MarbleLogoText} />
-    <h1 class="text-4xl font-bold text-white">A simple and powerful .NET language</h1>
-    <p class="text-2xl text-white text-center w-96">
+    <h1 class="text-4xl font-bold text-white text-center">A simple and powerful .NET language</h1>
+    <p class="text-2xl text-white text-center sm:w-96">
       Coming soon.<br>In the meantime, check out the GitHub for more information.<br>Or, contact us at the email below.
     </p>
     <div class="flex space-x-12">
