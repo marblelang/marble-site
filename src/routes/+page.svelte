313fd5c20d270115d41168db7e21505c11eacee1
Luis Luvia <lluvia@marblelang.net>
2023-08-30 19:07:17 -0500

    Add main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { Icon } from 'svelte-ionicons'
+  import MarbleLogoText from '$lib/assets/logo-with-text.svg'
+</script>
+
+<svelte:head>
+  <title>Marble Language</title>
+</svelte:head>
+
+<div class="w-screen h-screen bg-jet">
+  <div class="flex flex-col items-center justify-center h-full space-y-8">
+    <img alt="Marble logo" src={MarbleLogoText} />
+    <h1 class="text-4xl font-bold text-white">A simple and powerful .NET language</h1>
+    <p class="text-2xl text-white text-center w-96">
+      Coming soon.<br>In the meantime, check out the GitHub for more information.<br>Or, contact us at the email below.
+    </p>
+    <div class="flex space-x-12">
+      <a href="https://github.com/marblelang">
+        <Icon name="logo-github" width="96" height="96" class="text-white" />
+      </a>
+      <a href="mailto:contact@marblelang.net">
+        <Icon name="mail" width="96" height="96" class="text-white" />
+      </a>
+    </div>
+  </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cornflower': '#779cff',
+        'electric': '#7cf1fe',
+        'wisteria': '#d096e6',
+        'viridian': '#4c8577',
+        'jet': '#363636'
+      },
+      fontFamily: {
+        'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
   plugins: [],
 }
